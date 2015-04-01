75a012c7c241640107b214616018b08392df35ec
Yuom Theara <yuom.theara@gmail.com>
2015-04-01 18:09:28 +0700

    update company form

--- app/client/templates/company/company.js
+++ app/client/templates/company/company.js
@@ -1,9 +1,21 @@
 /**
+ * List
+ */
+Template.app_company.events({
+    'click .update': function (e, t) {
+        var data = App.Collection.Company.findOne();
+
+        ModalTemplate.show('app_companyUpdate', data);
+    }
+});
+
+/**
  * Hook
  */
 AutoForm.hooks({
-    app_company: {
+    app_companyUpdate: {
         onSuccess: function (formType, result) {
+            $('#app_companyUpdateModal').modal('hide');
             toastr.success(App.Message.success, 'Success');
         },
         onError: function (formType, error) {
